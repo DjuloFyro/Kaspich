cb248516e1d7ed5bd9692aed86e8854013769d10
julian.gil <juliangil2424@yahoo.fr>
2023-07-21 17:10:45 +0200

    update: perftree debugger

--- src/perftree.py
+++ src/perftree.py
@@ -1,17 +1,14 @@
 import numpy as np
 from board import Board
 from move import Move
-import utils
 from move_generation import *
 import sys
 
 def perft(board, depth):
     if depth == 0:
         return 1
-
     total_nodes = 0
-
-    for move in gen_legal_moves(board):
+    for move in generate_legal_moves(board):
         new_board = board.apply_move(move)
         total_nodes += perft(new_board, depth - 1)
 
@@ -32,7 +29,7 @@ def main():
     #if moves != []:
     #    moves = map(lambda m: Move.from_str(m), moves)
     #else:
-    moves = gen_legal_moves(board)
+    moves = generate_legal_moves(board)
 
     total_count = 0
     for move in moves:

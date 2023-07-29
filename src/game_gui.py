ffb892fa78522fc42f1ad6decf5cabf3bb8c76f7
julian.gil <juliangil2424@yahoo.fr>
2023-07-29 15:38:28 +0200

    add: cleaner code gor gui

--- src/game_gui.py
+++ src/game_gui.py
@@ -9,47 +9,69 @@ IMAGES = {}
 
 
 def load_images():
+    """Load chess piece images."""
     pieces = ["P", "R", "N", "B", "K", "Q", "p", "r", "n", "b", "k", "q"]
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("/home/djulo/Source/PersonnalProjects/Kaspich/src/images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
 
-def draw_game_state(screen, board):
+
+def draw_game_state(screen: p.Surface, board: Board):
+    """Draw the current game state on the screen.
+
+    Parameters:
+        screen (pygame.Surface): The screen to draw on.
+        board (Board): The chessboard state to be drawn.
+    """
     draw_board(screen)
     draw_pieces(screen, board)
 
 
-def draw_board(screen):
-    colors = [p.Color("white"), p.Color((50,50,50))]
+def draw_board(screen: p.Surface):
+    """Draw the chessboard squares on the screen.
+
+    Parameters:
+        screen (pygame.Surface): The screen to draw on.
+    """
+    colors = [p.Color("white"), p.Color((50, 50, 50))]
     for rank in range(DIMENSION):
         for file in range(DIMENSION):
-            color = colors[((rank + file ) % 2)]
-            p.draw.rect(screen, color, p.Rect(file*SQ_SIZE, rank*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            color = colors[((rank + file) % 2)]
+            p.draw.rect(screen, color, p.Rect(file * SQ_SIZE, rank * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
-def draw_pieces(screen, board):
+def draw_pieces(screen: p.Surface, board: Board):
+    """Draw chess pieces on the screen.
+
+    Parameters:
+        screen (pygame.Surface): The screen to draw on.
+        board (Board): The chessboard state with pieces to be drawn.
+    """
     for rank in range(DIMENSION - 1, -1, -1):
         for file in range(DIMENSION):
-            white_piece = board.piece_on(Square(rank*8 + file), color=Color.WHITE)
-            black_piece = board.piece_on(Square(rank*8 + file), color=Color.BLACK)
-            if white_piece != None:
-                screen.blit(IMAGES[white_piece.to_char().upper()], p.Rect(file*SQ_SIZE, (7-rank)*SQ_SIZE, SQ_SIZE, SQ_SIZE))
-            if black_piece != None:
-                screen.blit(IMAGES[black_piece.to_char()], p.Rect(file*SQ_SIZE, (7-rank)*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            white_piece = board.piece_on(Square(rank * 8 + file), color=Color.WHITE)
+            black_piece = board.piece_on(Square(rank * 8 + file), color=Color.BLACK)
+            if white_piece is not None:
+                screen.blit(IMAGES[white_piece.to_char().upper()], p.Rect(file * SQ_SIZE, (7 - rank) * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            if black_piece is not None:
+                screen.blit(IMAGES[black_piece.to_char()], p.Rect(file * SQ_SIZE, (7 - rank) * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 def main():
+    """Main function to run the chess game."""
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
 
     load_images()
-    
+
     board = Board()
     board.board_initialization()
 
-    sq_selected = None # Keep track of the last click of the user (rank, file)
-    player_clicks = []
+    selected_piece = None
+    possible_moves = []
+    dragging = False
+    x, y = 0, 0  # Initialize x and y outside the event loop
 
     running = True
     while running:
@@ -64,23 +86,38 @@ def main():
                 selected_piece_square = Square(rank * 8 + file)
                 if selected_piece is not None:
                     possible_moves = generate_legal_moves(board)
+                    dragging = True
 
             elif e.type == p.MOUSEBUTTONUP:
-                if selected_piece is not None:
+                if dragging and selected_piece is not None:
                     location = p.mouse.get_pos()
                     file = location[0] // SQ_SIZE
                     rank = 7 - (location[1] // SQ_SIZE)
                     target_square = Square(rank * 8 + file)
                     move = Move(selected_piece_square, target_square)
-                    
+
                     if move in possible_moves:
                         board = board.apply_move(move=move)
                     selected_piece = None
                     possible_moves = []
+                    dragging = False
+
+            elif e.type == p.MOUSEMOTION:
+                if dragging and selected_piece is not None:
+                    location = p.mouse.get_pos()
+                    x = location[0] - SQ_SIZE // 2
+                    y = location[1] - SQ_SIZE // 2
 
         draw_game_state(screen=screen, board=board)
+        if dragging:
+            if board.color_turn == Color.WHITE:
+                screen.blit(IMAGES[selected_piece.to_char().upper()], p.Rect(x, y, SQ_SIZE, SQ_SIZE))
+            else:
+                screen.blit(IMAGES[selected_piece.to_char()], p.Rect(x, y, SQ_SIZE, SQ_SIZE))
+
         clock.tick(MAX_FPS)
         p.display.flip()
+
 
 if __name__ == "__main__":
     main()
